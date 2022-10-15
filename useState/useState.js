a132b300932f2a46bd166fa09691f9db623a2567
prem <20bq1a05k7@vvit.net>
2022-10-15 04:09:40 +0000

    Update 2 files

--- useState/useState.js
+++ useState/useState.js
@@ -1,51 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function ViewItems() {
   const [item, setItem] = useState("");
   const [items, setItems] = useState([]);
   const handleAddItem = () => {
-    setItems([...items, {"item":item, isStriked:false}]);
+    setItems([...items, { item: item, isStriked: false }]);
     setItem("");
   };
-  const handleDelete = (item) => {
-    const fitems = items.filter((it) => it.item !== item.item);
-    setItems([...fitems]);
-  };
-  const handleText=(item)=>{
+  const handleDelete = (item) =>{
+    const fitems= item.filter((it) => it.item ! == item.item);
+    setItems([...fitems];)
+  }
+  const handleText = (item) => {
     const fitems = items.map((it) => {
-      if(it.item === item.item){
+      if (it.item === item.item) {
         return {
           ...it,
-          isStriked:!it.isStriked
-        }
-    } else {
-      return it;
-    }});
-    setItems([...fitems]);  }
-    
+          isStriked: !it.isStriked,
+        };
+      } else {
+        return it;
+      }
+    });
+    setItems([...fitems]);
+  };
+
   console.log(items);
-  const tdata = items.map((it, index) => (
-    <tr key={index}>
-      <td><span onClick={() =>handleText(it)} 
-      style={{textDecoration:it.isStriked? "line-through":"none"}}>{it.item}</span></td>
-      <td>
-        <button onClick={() => handleDelete(it)}>X</button>
-      </td>
-    </tr>
-  ),[]);
+  const tdata = items.map(
+    (it, index) => (
+      <tr key={index}>
+        <td>
+          <span
+            onClick={() => handleText(it)}
+            style={{ textDecoration: it.isStriked ? "line-through" : "none" }}
+          >
+            {it.item}
+          </span>
+        </td>
+        <td>
+          <button onClick={() => handleDelete(it)}>X</button>
+        </td>
+      </tr>
+    ),
+    []
+  );
   return (
     <div>
       <input
         type="text"
         size="20"
-        value={item}
+        valur={item}
         onChange={(e) => setItem(e.target.value)}
       />
       <button onClick={() => handleAddItem()}>Add</button>
       <h1>Items</h1>
-      <table border=" ">{tdata}</table>
+      <table border="">{tdata}</table>
     </div>
   );
 }
-
 export default ViewItems;
